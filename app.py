9c4904988b91c79fbc582049b112f24573b6fdf9
jimmydin7 <turnspeaker@gmail.com>
2025-10-15 16:36:06 +0300

    prod ports

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # Poetlog is a simple IP logger that generates poet lyrics with AI based on your information
+
+from flask import Flask, render_template, os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+
+    port = int(os.getenv("PORT", 4040))
+    app.run(host="0.0.0.0", port=port)
