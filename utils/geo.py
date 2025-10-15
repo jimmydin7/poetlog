d9c8f2bf3425b08b353e532235e3f71876324266
jimmydin7 <turnspeaker@gmail.com>
2025-10-15 17:24:30 +0300

    working!

--- utils/geo.py
+++ utils/geo.py
@@ -1,10 +1,10 @@
 import requests
 
-API_ENDPOINT = 'https://ipinfo.io/'
+API_ENDPOINT = 'https://ipinfo.io/2a02:586:451a:d600:6dba:b26:9ba9:879b/json'
 
 def request_geo(ip):
     
-    initial_response = requests.get(API_ENDPOINT+f'{str(ip)}/json', timeout=5)
+    initial_response = requests.get(API_ENDPOINT+f'{"[" + str(ip) + "]"}/json', timeout=5)
     response = initial_response.json()
 
     geo_data = {
