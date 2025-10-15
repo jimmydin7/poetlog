888328f48227df10ab7ac116eca2e1853603159e
jimmydin7 <turnspeaker@gmail.com>
2025-10-15 16:59:11 +0300

    getting ip

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # Poetlog is a simple IP logger that generates poet lyrics with AI based on your information
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
-
+#import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr) #get ip
+
     return render_template('index.html')
 
 if __name__ == "__main__":
