f781f2f603931aa161137d9e57396e67f0da87c2
jimmydin7 <turnspeaker@gmail.com>
2025-10-15 17:07:39 +0300

    geodata integration

---
+++ utils/geo.py
@@ -0,0 +1,19 @@
+import requests
+
+API_ENDPOINT = 'https://ipinfo.io/'
+
+def request_geo(ip):
+    
+    response = requests.get(API_ENDPOINT+f'/{ip}/json').json() 
+
+    geo_data = {
+
+        "city": response.get('city'),
+        "region": response.get('region'),
+        "country": response.get('country'),
+        "location": response.get('loc'),
+        "isp": response.get('org'),
+        "timezone":response.get('timezone')
+        
+        
+    }
